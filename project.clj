b84e4283763a509eef81c46c0b12cbacff4059f5
John Newby <john@newby.org>
2021-10-11 13:15:47 +0200

    Many new libs

--- project.clj
+++ project.clj
@@ -4,11 +4,28 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [
-                 [org.clojure/clojure "1.10.0"]
-                 [java-se/cgp.sample "0.8.3" ]
-                 [java-se/java-se.api "0.8.3"]
-                 [java-se/java-se.model "0.8.3"]
-                 [java-se/java-se.utils.assert "0.8.3" ] ]
+                [tezos/java-se.utils.assert "0.8.3"]
+                 [tezos/java-se.utils.string "0.8.3"]
+                 [tezos/java-se.utils.osgi "0.8.3"]
+                 [tezos/camel.sample.twitter "0.8.3"]
+                 [tezos/java-se.utils.io "0.8.3"]
+                 [tezos/java-se.papi "0.8.3"]
+                 [tezos/cgp.sample "0.8.3"]
+                 [tezos/java-se.default-impl "0.8.3"]
+                 [tezos/java-se.utils.bytes "0.8.3"]
+                 [tezos/cgp.tdc.generator.micheline "0.8.3"]
+                 [tezos/cgp-maven-plugin "0.8.3"]
+                 [tezos/cgp.java.generator.velocity "0.8.3"]
+                 [tezos/camel.sample.indexer "0.8.3"]
+                 [tezos/camel.sample "0.8.3"]
+                 [tezos/java-se.sample.fa12 "0.8.3"]
+                 [tezos/java-se.api "0.8.3"]
+                 [tezos/java-se.utils.micheline "0.8.3"]
+                 [tezos/camel.component "0.8.3"]
+                 [tezos/cgp.api "0.8.3"]
+                 [tezos/java-se.crypto-default-impl "0.8.3"]
+                 [tezos/java-se.model "0.8.3"]
+                 [tezos/java-se.proxy "0.8.3"] ]
 
   :main ^:skip-aot clj4tezos.core
   :target-path "target/%s"
