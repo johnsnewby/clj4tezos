a3818c0655287bbd403fe5bb625dce4b8b4a3074
John Newby <john@newby.org>
2021-10-12 11:17:49 +0100

    Gen Javadoc script

---
+++ gen-javadoc.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+
+
+for FILE in `find ej4tezos -name \*javadoc\*.jar`; do
+    BASENAME=`basename $FILE`
+    LIBNAME=`echo $BASENAME | awk -F'-1' '{print $1}'`
+    mkdir -p javadoc/$LIBNAME
+    cd javadoc/$LIBNAME
+    jar xf ../../$FILE
+    cd ../..
+done
