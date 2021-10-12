f0f78803c79e5d869e683e6656ea9ed2d9a026d1
John Newby <john@newby.org>
2021-10-12 16:10:56 +0100

    Javadoc added

--- gen-javadoc.sh
+++ gen-javadoc.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-
-
 for FILE in `find ej4tezos -name \*javadoc\*.jar`; do
     BASENAME=`basename $FILE`
     LIBNAME=`echo $BASENAME | awk -F'-1' '{print $1}'`
--- javadoc/java-se.model/package-search-index.js
+++ javadoc/java-se.model/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"org.ej4tezos.model"},{"l":"org.ej4tezos.model.exception"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"org.ej4tezos.model"}];updateSearchResults();
